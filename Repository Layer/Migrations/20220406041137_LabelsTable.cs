32bd438ec3d5f40b8293e5fc2ca96f2cab3ce458
nitishranjan27 <nk47595@gmail.com>
2022-04-06 09:55:51 +0530

    Nitish Creating and Migrating LabelTable in Label Program

---
+++ Repository Layer/Migrations/20220406041137_LabelsTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository_Layer.Migrations
+{
+    public partial class LabelsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LabelsTable",
+                columns: table => new
+                {
+                    LabelID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LabelName = table.Column<string>(nullable: true),
+                    Id = table.Column<long>(nullable: false),
+                    NoteId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabelsTable", x => x.LabelID);
+                    table.ForeignKey(
+                        name: "FK_LabelsTable_UserTable_Id",
+                        column: x => x.Id,
+                        principalTable: "UserTable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_LabelsTable_NotesTable_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "NotesTable",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabelsTable_Id",
+                table: "LabelsTable",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabelsTable_NoteId",
+                table: "LabelsTable",
+                column: "NoteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LabelsTable");
+        }
+    }
+}
