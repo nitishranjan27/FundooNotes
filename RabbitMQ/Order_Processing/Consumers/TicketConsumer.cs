a695fd229a2369d7f93ee8b01c2bc76fe29ea6a6
nitishranjan27 <nk47595@gmail.com>
2022-04-10 22:35:28 +0530

    Nitish Added the RabbitMQ in FundooNotes Program

---
+++ RabbitMQ/Order_Processing/Consumers/TicketConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using RabbitMQ.Model;
+using System.Threading.Tasks;
+
+namespace Order_Processing.Consumers
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
